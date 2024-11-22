f0cf7386bd758c5d8b3fe58064fb99c511d0ef5e
komaja@email.cz <komaja@email.cz>
2024-11-22 19:27:39 +0000

    Created Subform1 and Subform2; Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -4,6 +4,9 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
+from ..Subform2 import Subform2
+from ..Subform1 import Subform1
+
 
 class Form1(Form1Template):
   def __init__(self, **properties):
@@ -11,3 +14,18 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+    self.navigation_link_1.
+
+  def navigation_link_1_click(self, **event_args):
+    """This method is called when the component is clicked"""
+    self.content.clear()
+    self.content.add_component(Subform1())
+    self.navigation_link_1.selected = True
+    self.navigation_link_2.selected = False
+
+  def navigation_link_2_click(self, **event_args):
+    """This method is called when the component is clicked"""
+    self.content.clear()
+    self.content.add_component(Subform2())
+    self.navigation_link_1.selected = False
+    self.navigation_link_2.selected = True
